e5b8f2d56b6da77e26b986994f099d80a0c294f7
okmanek <okmanek@onet.eu>
2020-06-27 19:53:47 +0200

    DevTools disabled in prod

--- main.js
+++ main.js
@@ -74,3 +74,22 @@ const mainMenuTemplate = [ // just an array of objects
     ]
   }
 ];
+
+// Add developer tools item if not in prod
+if(process.env.NODE_ENV !== 'production') {
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+	 label: 'Toggle DevTools',
+	 accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+	 click(item, focusedWindow) {
+	   focusedWindow.toggleDevTools();
+	 }
+      },
+      {
+	 role: 'reload'
+      }
+    ]
+  });
+}
