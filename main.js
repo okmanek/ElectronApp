cdf9e617b383870b524c36846708db1034a629fe
okmanek <okmanek@onet.eu>
2020-06-27 19:31:17 +0200

    added adding and closing window

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const path = require('path');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for app to be ready
 app.on('ready', function() {
@@ -17,15 +18,59 @@ app.on('ready', function() {
     slashes: true
   })); // fancy way of passing this:	file://dirname//mainWindow.html
 
+  // Quit whole app when main window closed
+  mainWindow.on('closed', function(){
+    app.quit();
+  });
+
   // Build menu from template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   // Insert menu
   Menu.setApplicationMenu(mainMenu);
 });
 
+// Handle create add window
+function createAddWindow() {
+  // Create new window
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add Shopping List Item'
+  });
+
+  // Garbage collection handle
+  addWindow.on('close', function(){
+    addWindow = null;
+  });
+
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol:'file:',
+    slashes: true
+  })); // fancy way of passing this:	file://dirname//mainWindow.html
+}
+
 // Create menu template
 const mainMenuTemplate = [ // just an array of objects
   {
-    label:'File'
+    label:'File',
+    submenu: [
+      {
+	 label: 'Add item',
+	 click() {
+	   createAddWindow();
+	 }
+      },
+      {
+	 label: 'Clear items'
+      },
+      {
+	 label: 'Quit',
+	 accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+	 click() {
+	   app.quit();
+	 }
+      }
+    ]
   }
 ];
